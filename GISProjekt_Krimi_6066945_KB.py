a88323da817585bb028bf26c65dcc7108c11435b
KB <kbarginda@outlook.com>
2021-03-31 22:58:03 +0200

    DONE

--- GISProjekt_Krimi_6066945_KB.py
+++ GISProjekt_Krimi_6066945_KB.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  7 17:45:17 2021
+KAYLA BARGINDA (6066945)
 
-@author: Kayla
+Geo-M-303-100 GIS-Programmierung
+
+
+
+BKA Daten:
+https://www.bka.de/DE/AktuelleInformationen/StatistikenLagebilder/PolizeilicheKriminalstatistik/PKS2018/pks2018_node.html
+	Quellenangabe:(PKS Bundeskriminalamt, 2018, PKS-Tabellen 2018: F-03-T01-Kreise)
+	Datei: BKA-LKS-F-03-T01-Kreise_csv.csv 
+
+Landkreisdaten:
+http://opendatalab.de/projects/geojson-utilities/#
+
+NUTS-Klassifikationsdaten: 
+(Eurostat)
+https://www.esf.de/portal/SharedDocs/PDFs/DE/FP%202014-2020/nuts-klassifikation.pdf?__blob=publicationFile&v=4
+
+Bevölkerungsdaten:
+https://www.destatis.de
+
 """
 
 import sys, os, json, geojson
@@ -17,7 +35,6 @@ from shapely.geometry import shape, GeometryCollection, Point, MultiPoint
 from shapely.ops import nearest_points
 import folium
 
-from haversine import haversine
 from folium.plugins.draw import Draw
 
 from PyQt5.QtWidgets import QApplication, QWidget, QDockWidget, QHBoxLayout, QVBoxLayout, QBoxLayout, QSlider,QTableView, QLabel, QPlainTextEdit, QPushButton
@@ -30,72 +47,68 @@ from PyQt5.QtCore import Qt, QAbstractTableModel
 '''
 Datenverwaltung
 
+**ALLE DATEN SIND GLOBAL**
+
 '''
 
-#RENAME TO CAP NAMES BC GLOBAL 
+#PATHs
+PATH = os.getcwd()
 
-#Paths
-path = os.getcwd()
+GEO_DATEN = PATH + "/Data/landkreise_DE.geojson"
+BEV_DATEN = PATH + "/Data/bev_landkreis2.csv"
 
-geo_daten = path + "/Data/Landkreise_DE.geojson"
-bev_daten = path + "/Data/bev_landkreis2.csv"
-
-#Geodaten: Landkreise + BKA Daten (JOINED)
-df = gpd.read_file(geo_daten)
-df.head()
+#Geodaten: LANDKREISE + BKA Daten (JOINED)
+DF = gpd.read_file(GEO_DATEN)
+DF.head()
 
 
 #Relevante Daten aus dem Dataframe laden 
-landkreise = df[['GEN', 'geometry']]
-landkreise.set_index('GEN')
-landkreise.head()
+LANDKREISE = DF[['GEN', 'geometry']]
+LANDKREISE.set_index('GEN')
+LANDKREISE.head()
 
-#Stats
-df_2 = pd.read_csv(bev_daten, delimiter=";")
-df_2.head()
+#KRIMI_STATS
+DF_2 = pd.read_csv(BEV_DATEN, delimiter=";")
+DF_2.head()
 
 #Kriminalitätsdaten 
-krimi_daten = df_2.copy()
-krimi_daten.head()
+KRIMI_DATEN = DF_2.copy()
+KRIMI_DATEN.head()
+
+KRIMI_STATS = KRIMI_DATEN
+KRIMI_STATS['krimi'] = round((KRIMI_DATEN['erfasste_Faelle']/KRIMI_DATEN['bev']) * 100000,0)
+
+GEO_DATEN_TEMP = DF.copy()
+GEO_DATEN_TEMP2 = GEO_DATEN_TEMP[['NUTS', 'geometry']]
+KRIMI_STATS2 = KRIMI_STATS[['NUTS', 'krimi']]
+
+GEO_KRIMI = GEO_DATEN_TEMP2.merge(KRIMI_STATS2, how='inner', on='NUTS')
 
 
-stats = krimi_daten
+#Mittelpunkte der Polygone (pro LANDKREISE)
+LANDKREIS_PUNKTE = DF[['GEN', 'NUTS', 'geometry']].copy()
+LANDKREIS_PUNKTE['geometry'] = LANDKREIS_PUNKTE['geometry'].centroid
+LANDKREIS_PUNKTE.head()
 
-stats['krimi'] = round((krimi_daten['erfasste_Faelle']/krimi_daten['bev']) * 100000,0)
-
-geoDaten = df.copy()
-geoDaten2 = geoDaten[['NUTS', 'geometry']]
-
-stats2 = stats[['NUTS', 'krimi']]
-
-geoKrimi = geoDaten2.merge(stats2, how='inner', on='NUTS')
-
-
-#Mittelpunkte der Polygone (pro Landkreise)
-punkte = df[['GEN', 'NUTS', 'geometry']].copy()
-punkte['geometry'] = punkte['geometry'].centroid
-punkte.head()
-
-POIS = punkte['geometry']
+POIS = LANDKREIS_PUNKTE['geometry']
 POIS = MultiPoint(POIS)
 
 
-#SET GLOBALS 
-point = None
-currentLocation = None
-nearestRegion = None
-dist = None
-krimiStats = None
-total = None 
+#GLOBALE VARIABLEN
+CURRENT_POINT = None
+CURRENT_LOC = None
+NEAREST_LOC = None
+DIST = None
+#krimiKRIMI_STATS = None
+KRIMI_GESAMT = None 
 
-#basiskarte: choroplethenkarte
+#benutzerdefinierte Choroplethenkarte
 def choroplethenKarte(k):
         
-        
-        #set for custom choropleth
-        krimiDict = stats.set_index("NUTS")['krimi']
 
-        #check and get rid of any spaces 
+        krimiDict = KRIMI_STATS.set_index("NUTS")['krimi']
+
+        #löscht alle Leerzeichen in den Daten
         for key in krimiDict.keys():
             s = key.replace(' ', '')
             krimiDict[s] = krimiDict[key]
@@ -113,7 +126,7 @@ def choroplethenKarte(k):
             elif (krimiWert > 6700 and krimiWert < 8900):
                 color = '#c994c7'
             elif (krimiWert > 8900 and krimiWert < 11000):
-                color = '#df65b0'
+                color = '#DF65b0'
             elif (krimiWert > 11000 and krimiWert < 13400):
                 color = '#dd1c77'
             elif (krimiWert > 13400):
@@ -126,8 +139,8 @@ def choroplethenKarte(k):
                                                  "weight": 1,
                                                  "line_opacity":0.2}
             
-        folium.GeoJson(geo_daten, 
-               name="Landkreise",
+        folium.GeoJson(GEO_DATEN, 
+               name="LANDKREISE",
                show = True,
                tooltip = folium.features.GeoJsonTooltip(fields = ['GEN'],
                                                         aliases = ['Landkreis: '],
@@ -139,7 +152,7 @@ def choroplethenKarte(k):
         k.save("map.html")
 
 
-
+#Tabelle für PyQT. Stellt eine Tabelle da, mit Kriminalitätsstatistiken 
 class KrimiTabelle(QAbstractTableModel):
     
     def __init__(self, data):
@@ -164,71 +177,73 @@ class KrimiTabelle(QAbstractTableModel):
         return None
     
     
-#application widnow
+#Applikationsfenster
 class Karte(QtWidgets.QMainWindow):
     def __init__(self):
       
-        #initialise class
         super().__init__()
         self.initWindow()
 
-    #set and define window 
+    #definiert das Fenster
     def initWindow(self): 
         self.setWindowTitle('Kriminalität in Deutschland')
         self.kartenUI()
         self.showMaximized()
         
-
-            
+    #UI für Karte + Berechnungen 
     def kartenUI(self):
        
         '''
-        ADD KRIMI STATS TABLE
+        ADD KRIMI KRIMI_STATS TABLE
         '''
-        windowTabelle = stats[['GEN', 'bev', 'erfasste_Faelle', 'krimi']].copy()
+        windowTabelle = KRIMI_STATS[['GEN', 'bev', 'erfasste_Faelle', 'krimi']].copy()
         
         def getCoords(coords):
             #print(coords)
-            global point
-            point = Point(coords)#Convert to shapely point
+            global CURRENT_POINT
+            CURRENT_POINT = Point(coords)#Convert to shapely point
             
 
-            polygon = df.loc[df['geometry'].contains(point), 'GEN'].item()
+            polygon = DF.loc[DF['geometry'].contains(CURRENT_POINT), 'GEN'].item()
             
-            global currentLocation
+            global CURRENT_LOC
             
-            currentLocation = polygon
+            CURRENT_LOC = polygon
 
             
-            nearest_geom = nearest_points(point, POIS)
+            nearest_geom = nearest_points(CURRENT_POINT, POIS)
             
             for geom in nearest_geom: 
-                global nearestRegion
-                global dist
-                nearestRegion = df.loc[df['geometry'].contains(geom), 'GEN'].item()
+                global NEAREST_LOC
+                global DIST
+                NEAREST_LOC = DF.loc[DF['geometry'].contains(geom), 'GEN'].item()
 
            
             distances = []
-            for i in geoKrimi.values:
-                dist = round(point.distance(i[1]),2)
-                distances.append(dist)
+            for i in GEO_KRIMI.values:
+                DIST = round(CURRENT_POINT.distance(i[1]),2)
+                distances.append(DIST)
   
-            krimiWerte = geoKrimi['krimi'].to_numpy() #convert to numpy
+            krimiWerte = GEO_KRIMI['krimi'].to_numpy()
             
             
-            """
-            CALC KRIMI WERT
-            """
-            R = 0.7 #decayrate, ca. 80 km 
+            '''
+            Gewichtete Funktion, die die gesamte (gewichtete) Kriminalität in Deutschland zu einem bestimmten Zeitpunkt berechnet.
+            R = 1, Entspricht ca. 100 km (Zerfallsrate)
+            '''
+            
+            R = 1 #Zerfallsrate, ca. 100 km 
 
             weights = np.exp(-1*np.asarray(distances) / R)
             weights = weights / np.sum(weights)
             
-            global total
-            total = round(np.sum(weights * np.asarray(krimiWerte)),0)
+            global KRIMI_GESAMT
+            KRIMI_GESAMT = round(np.sum(weights * np.asarray(krimiWerte)),0)
 
         
-        """ LABELS ETC FOR GUI """
+        '''
+        Layout-Definitionen für PyQt-Widgets 
+        '''
 
         appText = QLabel()
         
@@ -239,21 +254,21 @@ class Karte(QtWidgets.QMainWindow):
         infos = QLabel()   
         outputText = QLabel() 
 
-        infos.setText("Der Puffer des Punktes beträgt ca. 80 km (1 Grad lon/lat).\nDie berechnete Gesamtkriminalität nimmt exponentiell in Bezug auf den gesetzten Puffer ab.")
+        infos.setText("Der Puffer des Punktes beträgt ca. 100 km (1 Grad lon/lat).\nDie berechnete Gesamtkriminalität nimmt exponentiell in Bezug auf den gesetzten Puffer ab.")
         
         
         def changeText(event):
-            outputText.setText("Aktuelle Koordinaten: " + str(point) +
-                               "\n" + "Aktueller Landkreis: " + str(currentLocation) + "\n"
-                               "Nächstgelegene Landkreise: " + str(nearestRegion) + "\n" + 
-                               "Gewichtete Gesamtkriminalität: " + str(total))
+            outputText.setText("Aktuelle Koordinaten: " + str(CURRENT_POINT) +
+                               "\n" + "Aktueller Landkreis: " + str(CURRENT_LOC) + "\n"
+                               "Nächstgelegene LANDKREISE: " + str(NEAREST_LOC) + "\n" + 
+                               "Gewichtete Gesamtkriminalität: " + str(KRIMI_GESAMT))
     
         button = QPushButton('Berechnen!')
         button.setFixedSize(100,32)
         button.clicked.connect(changeText)
         
 
-        #defines my QT Table
+        #Setzt die Anzeigeparameter der QT-Tabelle
         self.table = QtWidgets.QTableView()
         self.model = KrimiTabelle(windowTabelle)
         self.table.setModel(self.model)
@@ -261,42 +276,37 @@ class Karte(QtWidgets.QMainWindow):
         self.table.setColumnWidth(2, 120)
         self.table.setFixedSize(550,200)
     
-        
-        
         self.view = QtWebEngineWidgets.QWebEngineView()
         self.view.setContentsMargins(50, 50, 50, 50)
 
-        centralWindow = QtWidgets.QWidget() #main widget viewport
+        centralWindow = QtWidgets.QWidget() #Hauptwidget viewport
         self.setCentralWidget(centralWindow)
-        layout = QtWidgets.QVBoxLayout(centralWindow)   #create vertical layout
+        layout = QtWidgets.QVBoxLayout(centralWindow) #Vvrtikales Layout (für Widgets)
         
-        widget_container = QtWidgets.QWidget() #creates a container for my widgets
-        hLayout = QtWidgets.QHBoxLayout(widget_container)   #creates a horizontal container for my widgets
+        widget_container = QtWidgets.QWidget() #Widget-Behälter
+        hLayout = QtWidgets.QHBoxLayout(widget_container)  #horizontales Layout (für Widgets)
         
-        internalLayout = QtWidgets.QGridLayout() #creates grid layout within the horizontal layout 
-        hLayout.addLayout(internalLayout)   #add my grid box to the horizontal layout out(layout within layout)
-
+        internalLayout = QtWidgets.QGridLayout() #Gitter-Layout im horizontalen Layout
+        hLayout.addLayout(internalLayout)   #Gitter-Layout wird zum horizontalen Layout hinzugefügt
         
         internalLayout.addWidget(appText)
-        internalLayout.addWidget(infos)
-        internalLayout.addWidget(outputText)
-        internalLayout.addWidget(button)
+        internalLayout.addWidget(infos) #
+        internalLayout.addWidget(outputText) #Output Text 
+        internalLayout.addWidget(button) #Butten - "Berechnen"
 
-        
-        
-        hLayout.addWidget(self.table) # add krimi table
-         
+        hLayout.addWidget(self.table) #Krimitabelle
         layout.addWidget(widget_container)
 
-
-#ADD LEGEND!?!?!?!?
-        bildLabel = QLabel()
-        legende = QPixmap(path + 'legende.jpg')
-        bildLabel.setPixmap(legende)
+        imagePATH = PATH + '/Data/legende.png'
+        image = QLabel()
         
-        dock = QDockWidget("Legende")
-        dock.setGeometry(100,100,500,400)
-        dock.setWidget(bildLabel)
+        legende = QPixmap(imagePATH)
+        image.setPixmap(legende)
+        legende = legende.scaled(20,20, Qt.KeepAspectRatio)
+        dock = QDockWidget("Legende", self)
+        #dock.setGeometry(0,0,0,0)
+        
+        dock.setWidget(image)
         dock.setFloating(True)
 
         '''
@@ -323,25 +333,23 @@ class Karte(QtWidgets.QMainWindow):
             )
         drawPOI.add_to(k)
         
-        choroplethenKarte(k) #add choropleth map
+        choroplethenKarte(k) #Choroplethenkarte
     
-        folium.LayerControl().add_to(k) #layer control
+        folium.LayerControl().add_to(k) #Layerkontrolle 
 
-   
         '''
-        SAVE HTML MAP TO OPEN IN QT ENGINE 
+        PyQt Layer-Einstellungen 
         '''
-
-        temp_file = QtCore.QTemporaryFile("tempMap.html", self)
-    
         
+        #Speichert die Karte als HTML damit PyQt die Karten darstellen kann
+        temp_file = QtCore.QTemporaryFile("tempMap.html", self)
+
         if temp_file.open():
             k.save(temp_file.fileName())
             url = QtCore.QUrl.fromLocalFile(temp_file.fileName())
             
-            '''
-            GET POINT JS VALUE 
-            '''            
+         
+            #Ruft den JS-Punktwert ab
             class WebEnginePage(QtWebEngineWidgets.QWebEnginePage):
 
                 def javaScriptConsoleMessage(self, level, msg, line, sourceID):
@@ -349,16 +357,19 @@ class Karte(QtWidgets.QMainWindow):
                     temp_coordinateStorage = coords_dict['geometry']['coordinates']
                     
                     getCoords(temp_coordinateStorage)
-  
+            
+            #Für die Hauptseite
             view = QtWebEngineWidgets.QWebEngineView()
             page = WebEnginePage(view)
             view.setPage(page)
             view.load(url)
-
+        
         layout.addWidget(view)
 
  
-#GUI
+'''
+Führt die PyQt Webengine-Schnittstelle aus
+'''
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setStyleSheet('''
